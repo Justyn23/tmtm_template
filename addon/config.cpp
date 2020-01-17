337a42f58bc0f4c2f6318be8270af8de453ce3e1
Superxpdude <superxpdude@gmail.com>
2020-01-17 15:34:27 -0600

    Added the initial config.cpp file.

---
+++ addon/config.cpp
@@ -0,0 +1,23 @@
+class CfgPatches
+{
+	class xp_template
+	{
+		// Default BIS info
+		name = "XP Template";
+		author = "Superxpdude";
+		url = "https://github.com/Superxpdude/tmtm_template";
+		
+		requiredAddons[] = {"CBA_settings"};
+		requiredVersion = 1.94;
+		units[] = {};
+		weapons[] = {};
+		magazines[] = {};
+		
+		// CBA Info
+		authors[] = {"Superxpdude"};
+		version = 0.6;
+		versionStr = "0.6.0";
+		versionAr[] = {0,6,0};
+	};
+};
+
